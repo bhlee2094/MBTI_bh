ced101ad8285c45186f0cf2653c033b69b34730d
bhlee <djbh123@naver.com>
2021-01-28 21:31:50 +0900

    chatActivity 구현 완성

---
+++ app/src/main/java/kr/hongik/mbti/chatAdapter.java
@@ -0,0 +1,60 @@
+package kr.hongik.mbti;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class chatAdapter extends RecyclerView.Adapter<chatAdapter.ViewHolder> {
+
+    private ArrayList<Chat> localDataSet;
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textView;
+
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+
+            textView = (TextView) view.findViewById(R.id.tvChat);
+        }
+
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+
+    public chatAdapter(ArrayList<Chat> dataSet) {
+        localDataSet = dataSet;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.text_row_item, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.getTextView().setText(localDataSet.get(position).getText());
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return localDataSet.size();
+    }
+}
